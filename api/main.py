0541f2c598999499c28b26de536e2bbcbe684209
cyrilnwoba <cyrilnwoba@gmail.com>
2025-09-06 01:16:43 +0100

    Register and login api

--- api/main.py
+++ api/main.py
@@ -1,4 +1,37 @@
+#REGISTER
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
 
+    if not username or not password:
+        return jsonify({'msg': 'Missing username or password'}), 400
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({'msg': 'User already exists'}), 400
+
+    new_user = User(username=username, password_hash=generate_password_hash(password))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'msg': 'User registered successfully'}), 201
+#LOGIN
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'msg': 'Missing username or password'}), 400
+
+    user = User.query.filter_by(username=username).first()
+    if not user or not check_password_hash(user.password_hash, password):
+        return jsonify({'msg': 'Invalid username or password'}), 401
+
+    access_token = create_access_token(identity=user.id)
+    return jsonify({'access_token': access_token}), 200
 
 
 
